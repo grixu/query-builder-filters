41c7fe88dbfa11cede3736167836f852c9c6ed6e
Mateusz Gostański <mg@grixu.dev>
2021-02-15 13:15:47 +0100

    Added checking empty strings in IsNullFilter and NotNullFilter

--- src/IsNullFilter.php
+++ src/IsNullFilter.php
@@ -5,14 +5,13 @@ namespace Grixu\QueryBuilderFilters;
 use Illuminate\Database\Eloquent\Builder;
 use Spatie\QueryBuilder\Filters\Filter;
 
-/**
- * Class IsNullFilter
- * @package Support\CustomFilters
- */
 class IsNullFilter implements Filter
 {
     public function __invoke(Builder $query, $value, string $property)
     {
-        $query->whereNull($value);
+        $query->where(function($query) {
+            return $query->orWhereNull('ean')
+                ->orWhere('ean', '');
+        });
     }
 }
--- src/NotNullFilter.php
+++ src/NotNullFilter.php
@@ -5,14 +5,13 @@ namespace Grixu\QueryBuilderFilters;
 use Illuminate\Database\Eloquent\Builder;
 use Spatie\QueryBuilder\Filters\Filter;
 
-/**
- * Class NotNullFilter
- * @package Support\CustomFilters
- */
 class NotNullFilter implements Filter
 {
     public function __invoke(Builder $query, $value, string $property)
     {
-        $query->whereNotNull($value);
+        $query->where(function($query) {
+            return $query->whereNotNull('ean')
+                ->where('ean', '!=', '');
+        });
     }
 }
